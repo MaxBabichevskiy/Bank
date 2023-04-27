fbfc989c3de616edc1f975631cd5db4e6fa93421
Max Babichevskyi <100165364+MaxBabichevskiy@users.noreply.github.com>
2023-04-27 11:07:53 +0300

    Add files via upload

---
+++ ConsoleApp23/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class ApplicationContext : DbContext
+    {
+        public DbSet<Account> Accounts => Set<Account>();
+
+        public ApplicationContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = dbBank.db");
+        }
+    }
+}
---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,18 @@
+﻿using Bank;
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    Account Tom = new Account { Name = "Tom", Surname = "Parker", Money = 10234, Role = "User" };
+    Account Jessy = new Account { Name = "Jessy", Surname = "Rodrigez", Money = 567, Role = "User" };
+    Account Daniel = new Account { Name = "Daniel", Surname = "Tompson", Money = 534732, Role = "User" };
+
+    db.Accounts.Add(Tom);
+    db.Accounts.Add(Jessy);
+    db.Accounts.Add(Daniel);
+    db.SaveChanges();
+    
+
+    var accounts = db.Accounts.ToList();
+    Console.WriteLine("Список объектов");
+    foreach (var user in accounts) Console.WriteLine($"{user.Id}.{user.Name} - {user.Surname} - {user.Money} - {user.Role}");
+}
